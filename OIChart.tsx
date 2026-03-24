75bce84733fb8b0a56127dfa23081e206464482b
macairehugo01-glitch <macairehugo01@gmail.com>
2026-03-24 11:11:34 +0100

    Delete OIChart.tsx

--- OIChart.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getRecentSetups, getStats } from '@/lib/store'
-
-export const dynamic = 'force-dynamic'
-
-export async function GET() {
-  return NextResponse.json({ setups: getRecentSetups(30), stats: getStats() })
-}
